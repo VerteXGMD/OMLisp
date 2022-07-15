dfcf84200c0b7a216cd6377bc81b1b03c487a267
Adi Salimgereev <adi.salimgereev25@fizmat.kz>
2022-07-16 03:51:07 +0600

    Add test for unexcepted token

--- src/test/java/org/vertex/omlisp/lexer/LexerTest.java
+++ src/test/java/org/vertex/omlisp/lexer/LexerTest.java
@@ -2,12 +2,25 @@ package org.vertex.omlisp.lexer;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.vertex.omlisp.token.Token;
 import org.vertex.omlisp.token.TokenKind;
 
 public class LexerTest {
     @Test
     public void testEOF() {
-        Assert.assertEquals(TokenKind.EOF, new Lexer(
-                "<input>", "\0").nextToken().getKind());
+        Token token = new Lexer(
+                "<input>", "\0").nextToken();
+
+        Assert.assertEquals(TokenKind.EOF, token.getKind());
+        Assert.assertEquals('\0', token.getValue().getCharacterValue());
+    }
+
+    @Test
+    public void testUnexceptedToken() {
+        Token token = new Lexer(
+                "<input>", "[\0").nextToken();
+
+        Assert.assertEquals(TokenKind.UNEXCEPTED_CHARACTER_ERROR, token.getKind());
+        Assert.assertEquals('[', token.getValue().getCharacterValue());
     }
 }
